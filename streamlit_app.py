b1a9124b3bf9e948e868c578d6ca9c680c9684e4
ElPastaroni <162414445+ElPastaroni@users.noreply.github.com>
2024-05-27 22:20:08 +1200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,8 +9,8 @@ from matplotlib.ticker import StrMethodFormatter
 
 # Load the dataset
 @st.cache
-def load_data(file_path):
-    df = pd.read_excel(file_path)
+def load_data(Corelogic.xlsx):
+    df = pd.read_excel(Corelogic.xlsx)
     return df
 
 # Function to plot average house price per region
